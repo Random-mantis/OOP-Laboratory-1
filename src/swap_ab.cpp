e6c6a220fad466f2062f192512a2923a745d8a3b
Random-mantis <kstgnbk@gmail.com>
2025-10-02 11:01:58 +0300

    Add files via upload

--- src/swap_ab.cpp
+++ src/swap_ab.cpp
@@ -3,13 +3,14 @@
 #include <iostream>
 
 std::string swap_ab(const std::string s) {
+     
     std::string out;
     out.reserve(s.size());
-    for (char ch : s) {
-        if (ch == 'a'){ std::cout << 'b'; out.push_back('b');}
-        else if (ch == 'b') { std::cout << 'a'; out.push_back('a');}
-        else { std::cout << ch; out.push_back(ch);}
+    for (int i = 0; i < s.size() / sizeof(char); i++) {
+        if (s[i] == 'a'){ out.push_back('b');}
+        else if (s[i] == 'b') { out.push_back('a');}
+        else { out.push_back(s[i]);}
     }
-    std::cout << std::endl;
+    std::cout << out << std::endl; 
     return out;
 }
